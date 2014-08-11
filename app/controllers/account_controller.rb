51ced93b95c37d56d90bb4a66b9cc1c52438587c
Scott Morey <swmorey@gmail.com>
2014-08-11 20:01:22 +0000

    sort holds newest to oldest server side to preserve placement in view

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -168,7 +168,8 @@ class AccountController < ApplicationController
         :pickup_location => hold.css('td[5]').text.strip,
       }
     end
-    return holds
+    sorted_by_hold_id = holds.sort_by {|k| k["hold_id"]}.reverse!
+    return sorted_by_hold_id
   end
 
   def clean_record(string)
