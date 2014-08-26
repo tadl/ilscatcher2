66800427dd204a55af71588927b89ecabed3779a
Scott Morey <swmorey@gmail.com>
2014-08-26 18:46:46 +0000

    working for real now

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -8,7 +8,7 @@ class AccountController < ApplicationController
   	checkouts = page.parser.css('span#dash_checked').try(:text).strip
   	holds = page.parser.css('span#dash_holds').try(:text).strip
   	holds_ready = page.parser.css('span#dash_pickup').try(:text).strip
-  	fines = page.parser.css('span#dash_fines').try(:text).strip.gsub(/\$/, '')
+  	fines = page.parser.css('span#dash_fines').try(:text).strip
     card = page.at('td:contains("Active Barcode")').try(:next_element).try(:text)
   	if token == nil
   		render :json =>{:message => 'login failed'}
@@ -149,7 +149,6 @@ class AccountController < ApplicationController
         	:checkout_id => checkout.search('input[@name="circ"]').try(:attr, "value").to_s,
         	:renew_attempts => checkout.search('td[@name="renewals"]').text.to_s.try(:gsub!, /\n/," ").try(:squeeze, " ").try(:strip),
         	:due_date => checkout.search('td[@name="due_date"]').text.to_s.try(:gsub!, /\n/," ").try(:squeeze, " ").try(:strip),
-        	:iso_due_date => Date.parse(checkout.search('td[@name="due_date"]').text.to_s.try(:gsub!, /\n/," ").try(:squeeze, " ").try(:strip)).to_s,
         	:barcode => checkout.search('td[@name="barcode"]').text.to_s.try(:gsub!, /\n/," ").try(:squeeze, " ").try(:strip),
         }
     end
@@ -164,7 +163,7 @@ class AccountController < ApplicationController
         :record_id => clean_record(hold.css('td[2]').css('a').try(:attr, 'href').to_s),
         :hold_id => hold.search('input[@name="hold_id"]').try(:attr, "value").to_s,
         :hold_status => hold.css('td[8]').text.strip,
-        :queue_status => hold.css('/td[9]/div/div[1]').text.strip.gsub(/AvailableExpires/, 'Available, Expires'),
+        :queue_status => hold.css('/td[9]/div/div[1]').text.strip,
         :queue_state => hold.css('/td[9]/div/div[2]').text.strip,
         :pickup_location => hold.css('td[5]').text.strip,
       }
@@ -180,7 +179,8 @@ class AccountController < ApplicationController
   end
 
   def circ_to_title(page, circ_id)
-    title = page.at('input[@value="18599655"]').try(:parent).try(:next).try(:next).css('a')[0].try(:text)
+    look_for = 'input[@value="'+ circ_id +'"]'
+    title = page.at(look_for).try(:parent).try(:next).try(:next).try(:css, 'a')[0].try(:text)
     return title  
   end  
 
@@ -192,7 +192,7 @@ class AccountController < ApplicationController
         :checkouts => p.css('span#dash_checked').try(:text).strip, 
         :holds => p.css('span#dash_holds').try(:text).strip,
         :holds_ready => p.css('span#dash_pickup').try(:text).strip,
-        :fine => p.css('span#dash_fines').try(:text).strip.gsub(/\$/, ''),
+        :fine => p.css('span#dash_fines').try(:text).strip, 
         :token => token.try(:value),
         :card => p.at('td:contains("Active Barcode")').try(:next_element).try(:text)
       }
