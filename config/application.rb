eea50859ed9fc4e8670cab0981a26b1ca453b9ad
Scott Morey <swmorey@gmail.com>
2014-12-08 18:58:47 +0000

    also filtering md5 encrypted passwords

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ Bundler.require(*Rails.groups)
 module Ilscatcher2
   class Application < Rails::Application
     config.filter_parameters += [:password]
+    config.filter_parameters += [:pass_md5]
   end
 end
